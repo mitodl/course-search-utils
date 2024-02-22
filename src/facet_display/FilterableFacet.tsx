06726bb74dc3395a49770af9b0789815551e06d3
Anastasia Beglova <abeglova@mit.edu>
2024-02-22 10:37:48 -0500

    move filters to course search utils

---
+++ src/facet_display/FilterableFacet.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useCallback, useMemo } from "react"
+import { contains } from "ramda"
+import Fuse from "fuse.js"
+
+import { SearchFacetItem } from "./SearchFacetItem"
+import { BucketWithLabel } from "./FacetDisplay"
+
+// the `.search method returns records like { item, refindex }
+// where item is the facet and refIndex is it's index in the original
+// array. this helper just pulls out only the facets themselves
+const runSearch = (searcher: Fuse<BucketWithLabel>, text: string) =>
+  searcher.search(text).map(({ item }) => item)
+
+interface Props {
+  name: string
+  title: string
+  results: BucketWithLabel[] | null
+  selected: string[]
+  onUpdate: React.ChangeEventHandler<HTMLInputElement>
+  expandedOnLoad: boolean
+}
+
+function FilterableFacet(props: Props) {
+  const { name, title, results, selected, onUpdate, expandedOnLoad } = props
+  const [showFacetList, setShowFacetList] = useState(expandedOnLoad)
+
+  const [filterText, setFilterText] = useState("")
+
+  const handleFilterInput = useCallback(e => {
+    e.preventDefault()
+    const filterText = e.target.value || ""
+    setFilterText(filterText)
+  }, [])
+
+  const titleLineIcon = showFacetList ? "arrow_drop_down" : "arrow_right"
+
+  const filteredResults = useMemo(() => {
+    return filterText ?
+      runSearch(
+        new Fuse(results || [], { keys: ["key", "label"], threshold: 0.4 }),
+        filterText
+      ) :
+      results
+  }, [filterText, results])
+
+  const facets = (filteredResults || results) ?? []
+  return results && results.length === 0 ? null : (
+    <div className="facets filterable-facet">
+      <button
+        className="filter-section-button"
+        type="button"
+        aria-expanded={showFacetList ? "true" : "false"}
+        onClick={() => setShowFacetList(!showFacetList)}
+      >
+        {title}
+        <i className={`material-icons ${titleLineIcon}`} aria-hidden="true">
+          {titleLineIcon}
+        </i>
+      </button>
+      {showFacetList ? (
+        <>
+          <div className="input-wrapper">
+            <input
+              className="facet-filter"
+              type="text"
+              onChange={handleFilterInput}
+              value={filterText}
+              placeholder={`Search ${title || ""}`}
+            />
+            {filterText === "" ? (
+              <i
+                className="input-postfix-icon material-icons search-icon"
+                aria-hidden="true"
+              >
+                search
+              </i>
+            ) : (
+              <button
+                className="input-postfix-button"
+                type="button"
+                onClick={() => setFilterText("")}
+                aria-label="clear search text"
+              >
+                <span className="material-icons" aria-hidden="true">
+                  clear
+                </span>
+              </button>
+            )}
+          </div>
+          <div className="facet-list">
+            {facets.map((facet, i) => (
+              <SearchFacetItem
+                key={i}
+                facet={facet}
+                isChecked={contains(facet.key, selected || [])}
+                onUpdate={onUpdate}
+                name={name}
+                displayKey={facet.label}
+              />
+            ))}
+          </div>
+        </>
+      ) : null}
+    </div>
+  )
+}
+
+export default FilterableFacet
---
+++ src/facet_display/SearchFacetItem.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import Dotdotdot from "react-dotdotdot"
+import { Bucket } from "./types"
+
+interface Props {
+  facet: Bucket
+  isChecked: boolean
+  onUpdate: React.ChangeEventHandler<HTMLInputElement>
+  name: string
+  displayKey: string | null
+}
+
+export const slugify = (text: string) =>
+  text
+    .split(" ")
+    .map(subString => subString.toLowerCase())
+    .join("-")
+    .replace(/[\W_]/g, "-")
+
+export function SearchFacetItem(props: Props) {
+  const { facet, isChecked, onUpdate, name, displayKey } = props
+
+  const facetId = slugify(`${name}-${facet.key}`)
+  return (
+    <div className={isChecked ? "facet-visible checked" : "facet-visible"}>
+      <input
+        type="checkbox"
+        id={facetId}
+        name={name}
+        value={facet.key}
+        checked={isChecked}
+        onChange={onUpdate}
+      />
+      <div className="facet-label">
+        <label htmlFor={facetId} className={"facet-key"}>
+          <Dotdotdot clamp={1}>{displayKey || ""}</Dotdotdot>
+        </label>
+        <div className="facet-count">{facet.doc_count}</div>
+      </div>
+    </div>
+  )
+}
