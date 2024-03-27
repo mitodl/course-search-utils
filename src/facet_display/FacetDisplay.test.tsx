496287440d72040b802b998f4297779070fe57f3
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-03-27 13:37:53 -0400

    Include zero count active facets in facet options (#79)
    * remove enzyme from FacetDisplay test
    
    * automatically include zero-count active facets
    
    * remove commented out import
    
    * remove a console.log

--- src/facet_display/FacetDisplay.test.tsx
+++ src/facet_display/FacetDisplay.test.tsx
@@ -1,118 +1,200 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { render, screen, within } from "@testing-library/react"
+import user from "@testing-library/user-event"
 
-import {
-  default as FacetDisplay,
-  AvailableFacets,
-  getDepartmentName,
-  getLevelName
-} from "./FacetDisplay"
-import { FacetManifest, Facets } from "./types"
+import FacetDisplay, { getDepartmentName } from "./FacetDisplay"
+import type { FacetDisplayProps } from "./FacetDisplay"
+import { Bucket, FacetManifest, Facets } from "./types"
 
 describe("FacetDisplay component", () => {
-  const facetMap: FacetManifest = [
+  const defaultFacetMap: FacetManifest = [
     {
       name:               "topic",
       title:              "Topics",
       useFilterableFacet: false,
-      expandedOnLoad:     false
-    },
-    {
-      name:               "resource_type",
-      title:              "Types",
-      useFilterableFacet: false,
-      expandedOnLoad:     false
+      expandedOnLoad:     true
     },
     {
       name:               "department",
       title:              "Departments",
       useFilterableFacet: false,
-      expandedOnLoad:     true,
-      labelFunction:      getDepartmentName
-    },
-    {
-      name:               "level",
-      title:              "Level",
-      useFilterableFacet: false,
-      expandedOnLoad:     true,
-      labelFunction:      getLevelName
+      expandedOnLoad:     true
     }
   ]
 
-  function setup() {
+  const defaultAggregations = {
+    topic: [
+      { key: "Cats", doc_count: 10 },
+      { key: "Dogs", doc_count: 20 },
+      { key: "Monkeys", doc_count: 30 }
+    ],
+    department: [
+      { key: "1", doc_count: 100 },
+      { key: "2", doc_count: 200 }
+    ]
+  }
+
+  const setup = ({
+    props,
+    aggregations = defaultAggregations
+  }: {
+    props?: Partial<FacetDisplayProps>
+    aggregations?: Record<string, Bucket[]>
+  } = {}) => {
     const activeFacets = {}
-    const facetOptions = jest.fn()
+    const facetOptions = (group: string) => aggregations[group] ?? []
     const clearAllFilters = jest.fn()
     const onFacetChange = jest.fn()
 
-    const render = (props = {}) =>
-      shallow(
+    const view = render(
+      <FacetDisplay
+        facetMap={defaultFacetMap}
+        facetOptions={facetOptions}
+        activeFacets={activeFacets}
+        clearAllFilters={clearAllFilters}
+        onFacetChange={onFacetChange}
+        {...props}
+      />
+    )
+    const spies = {
+      onFacetChange,
+      clearAllFilters
+    }
+    const rerender = (newProps: Partial<FacetDisplayProps>) => {
+      view.rerender(
         <FacetDisplay
-          facetMap={facetMap}
+          facetMap={defaultFacetMap}
           facetOptions={facetOptions}
           activeFacets={activeFacets}
           clearAllFilters={clearAllFilters}
           onFacetChange={onFacetChange}
           {...props}
+          {...newProps}
         />
       )
-    return { render, clearAllFilters }
+    }
+    return { view, spies, rerender }
   }
 
-  test("renders a FacetDisplay with expected FilterableFacets", async () => {
-    const { render } = setup()
-    const wrapper = render()
-    const facets = wrapper.find(AvailableFacets).dive().children()
-    expect(facets).toHaveLength(4)
-    facets.map((facet, key) => {
-      expect(facet.prop("name")).toBe(facetMap[key].name)
-      expect(facet.prop("title")).toBe(facetMap[key].title)
-      expect(facet.prop("expandedOnLoad")).toBe(facetMap[key].expandedOnLoad)
-    })
+  test("Renders facets with expected titles", async () => {
+    setup()
+    screen.getByRole("button", { name: defaultFacetMap[0].title })
+    screen.getByRole("button", { name: defaultFacetMap[1].title })
   })
 
-  test("shows filters which are active", () => {
-    const activeFacets: Facets = {
-      topic:         ["Academic Writing", "Accounting", "Aerodynamics"],
-      resource_type: [],
-      department:    ["1", "2"]
+  test.each([{ expandedOnLoad: false }, { expandedOnLoad: true }])(
+    "Initially expanded based on expandedOnLoad ($expandedOnLoad)",
+    ({ expandedOnLoad }) => {
+      const facetMap = [{ ...defaultFacetMap[0], expandedOnLoad }]
+      setup({
+        props: { facetMap }
+      })
+      screen.getByRole("button", {
+        name:     facetMap[0].title,
+        expanded: expandedOnLoad
+      })
+      const checkboxes = screen.queryAllByRole("checkbox")
+      expect(checkboxes).toHaveLength(expandedOnLoad ? 3 : 0)
     }
+  )
 
-    const { render, clearAllFilters } = setup()
-    const wrapper = render({
-      activeFacets
+  test("Clicking facet title toggles expanded state", async () => {
+    setup({
+      props: { facetMap: [defaultFacetMap[0]] }
     })
-    expect(
-      wrapper
-        .find(".active-search-filters")
-        .find("SearchFilter")
-        .map(el => el.prop("value"))
-    ).toEqual(["Academic Writing", "Accounting", "Aerodynamics", "1", "2"])
-    wrapper.find(".clear-all-filters-button").simulate("click")
-    expect(clearAllFilters).toHaveBeenCalled()
+    const button = screen.getByRole("button", {
+      name: defaultFacetMap[0].title
+    })
+    const checkboxCount = () => screen.queryAllByRole("checkbox").length
+    await user.click(button)
+    expect(button.getAttribute("aria-expanded")).toBe("false")
+    expect(checkboxCount()).toBe(0)
+    await user.click(button)
+    expect(button.getAttribute("aria-expanded")).toBe("true")
+    expect(checkboxCount()).toBe(3)
+  })
+
+  test("Shows filters which are active", async () => {
+    const activeFacets: Facets = {
+      topic:      ["Cats", "Dogs"],
+      department: ["1"]
+    }
+    const { spies } = setup({ props: { activeFacets } })
+    const activeDisplay = document.querySelector(".active-search-filters")
+    if (!(activeDisplay instanceof HTMLElement)) {
+      throw new Error("Expected activeDisplay to exist")
+    }
+    within(activeDisplay).getByText("Cats")
+    within(activeDisplay).getByText("Dogs")
+    within(activeDisplay).getByText("1")
+
+    await user.click(screen.getByRole("button", { name: "Clear All" }))
+    expect(spies.clearAllFilters).toHaveBeenCalled()
+    await user.click(screen.getAllByRole("button", { name: "clear filter" })[0])
+    expect(spies.onFacetChange).toHaveBeenCalledWith("topic", "Cats", false)
+  })
+
+  test("Clicking a facet checkbox calls onFacetChange", async () => {
+    const { spies } = setup({
+      props: {
+        facetMap: [
+          {
+            ...defaultFacetMap[0],
+            expandedOnLoad: true
+          }
+        ]
+      }
+    })
+    await user.click(screen.getByRole("checkbox", { name: "Cats" }))
+    expect(spies.onFacetChange).toHaveBeenCalledWith("topic", "Cats", true)
   })
 
   test("it accepts a label function to convert codes to names", () => {
-    const activeFacets: Facets = {
-      topic:         [],
-      resource_type: [],
-      department:    ["1"],
-      level:         ["graduate"]
-    }
-
-    const { render, clearAllFilters } = setup()
-    const wrapper = render({
-      activeFacets
+    setup({
+      props: {
+        facetMap: [
+          {
+            ...defaultFacetMap[1],
+            labelFunction:  getDepartmentName,
+            expandedOnLoad: true
+          }
+        ]
+      }
     })
-    expect(
-      wrapper
-        .find(".active-search-filters")
-        .find("SearchFilter")
-        .map(el =>
-          el.render().find(".active-search-filter-label").first().html()
-        )
-    ).toEqual(["Civil and Environmental Engineering", "Graduate"])
-    wrapper.find(".clear-all-filters-button").simulate("click")
-    expect(clearAllFilters).toHaveBeenCalled()
+    screen.getByRole("checkbox", {
+      name: "Civil and Environmental Engineering"
+    })
+    screen.getByRole("checkbox", {
+      name: "Mechanical Engineering"
+    })
+  })
+
+  test("Automically includes a zero-count option for active facets with no matches", () => {
+    const activeFacets: Facets = {
+      topic: ["Cats"]
+    }
+    const { rerender } = setup({
+      props: {
+        activeFacets,
+        facetMap: [
+          {
+            ...defaultFacetMap[0],
+            expandedOnLoad: true
+          }
+        ]
+      }
+    })
+    const checkboxCount = () => screen.getAllByRole("checkbox").length
+    expect(checkboxCount()).toBe(3)
+    screen.getByRole("checkbox", { name: "Cats" })
+    screen.getByRole("checkbox", { name: "Dogs" })
+    screen.getByRole("checkbox", { name: "Monkeys" })
+    rerender({
+      activeFacets: {
+        topic: ["Cats", "Dragons"]
+      }
+    })
+    expect(checkboxCount()).toBe(4)
+    screen.getByRole("checkbox", { name: "Dragons" })
   })
 })
