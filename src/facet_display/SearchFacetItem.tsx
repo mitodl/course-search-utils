c940b68fd0be68207296b3b4cef37d20dcd928e6
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-05-10 15:31:19 -0400

    remove dotdotdot

--- src/facet_display/SearchFacetItem.tsx
+++ src/facet_display/SearchFacetItem.tsx
@@ -1,26 +1,25 @@
-import React from "react"
-import Dotdotdot from "react-dotdotdot"
-import { Bucket } from "./types"
+import React from "react";
+import { Bucket } from "./types";
 
 interface Props {
-  facet: Bucket
-  isChecked: boolean
-  onUpdate: React.ChangeEventHandler<HTMLInputElement>
-  name: string
-  displayKey: string | null
+  facet: Bucket;
+  isChecked: boolean;
+  onUpdate: React.ChangeEventHandler<HTMLInputElement>;
+  name: string;
+  displayKey: string | null;
 }
 
 export const slugify = (text: string) =>
   text
     .split(" ")
-    .map(subString => subString.toLowerCase())
+    .map((subString) => subString.toLowerCase())
     .join("-")
-    .replace(/[\W_]/g, "-")
+    .replace(/[\W_]/g, "-");
 
 export function SearchFacetItem(props: Props) {
-  const { facet, isChecked, onUpdate, name, displayKey } = props
+  const { facet, isChecked, onUpdate, name, displayKey } = props;
 
-  const facetId = slugify(`${name}-${facet.key}`)
+  const facetId = slugify(`${name}-${facet.key}`);
   return (
     <div className={isChecked ? "facet-visible checked" : "facet-visible"}>
       <input
@@ -33,10 +32,10 @@ export function SearchFacetItem(props: Props) {
       />
       <div className="facet-label">
         <label htmlFor={facetId} className={"facet-key"}>
-          <Dotdotdot clamp={1}>{displayKey || ""}</Dotdotdot>
+          {displayKey ?? ""}
         </label>
         <div className="facet-count">{facet.doc_count}</div>
       </div>
     </div>
-  )
+  );
 }
