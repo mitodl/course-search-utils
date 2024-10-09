49d11b721b86927079ee468b58412856acd81290
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-10-09 18:20:38 +0200

    Remove Enzyme

--- src/test_setup.ts
+++ src/test_setup.ts
@@ -1,13 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-import Enzyme from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("jest-fetch-mock").enableMocks()
-
-Enzyme.configure({ adapter: new Adapter() })
-
 process.env = {
   ...process.env,
   SEARCH_API_URL: "http://search-the-planet.example.com/search"
