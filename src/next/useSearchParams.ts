2be637effa7e3bdaf9adcbf98a04724cba2753b1
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-10-10 14:24:26 -0400

    update next useSearchParams hook to be client-side only

--- src/next/useSearchParams.ts
+++ src/next/useSearchParams.ts
@@ -1,54 +1,66 @@
-import { useCallback, useEffect, useMemo, useRef } from "react";
-import { useSearchParams as useNextSearchParams } from "next/navigation";
+import { useCallback, useEffect, useRef } from "react"
+import { useSearchParams as useNextSearchParams } from "next/navigation"
 
 type SearchParamsSetterValue =
   | URLSearchParams
-  | ((prevSearchParams: URLSearchParams) => URLSearchParams);
+  | ((prevSearchParams: URLSearchParams) => URLSearchParams)
 
-type SetSearchParams = (newSearchParams: SearchParamsSetterValue) => void;
+type SetSearchParams = (newSearchParams: SearchParamsSetterValue) => void
 
-const useSearchParams = (): [URLSearchParams, SetSearchParams] => {
-  const search = useNextSearchParams();
-
+/**
+ * Returns a function for setting search params in the browser URL.
+ *  - the first call within a single render cycle will add a new entry in the
+ *   history stack via pushState
+ *  - subsequent calls will replace the previous entry in the history stack
+ *    and will receive the previously updated value in setter function
+ *
+ * NOTE: This is intended for use with NextJS, but could work with any framework
+ * that supports direct usage of window.history.pushState / replaceState.
+ */
+const useSetSearchParams = (): SetSearchParams => {
   /**
    * Keep track of whether navigate has been called in the current render cycle
    * to avoid adding extra entries in the history stack.
    */
-  const hasNavigatedRef = useRef(false);
-  const searchParams = useMemo(() => new URLSearchParams(search), [search]);
-  /**
-   * Keep track of the current searchParams value so that updater functions can
-   * use the current value rather than value from previous render.
-   */
-  const searchParamsRef = useRef(searchParams);
+  const hasNavigatedRef = useRef(false)
 
   useEffect(() => {
-    hasNavigatedRef.current = false;
-    /**
-     * Each render, sync the ref with the current state value.
-     * This is necessary in case search params has changed via some source other
-     * than this hook (e.g., browser navigation).
-     */
-    searchParamsRef.current = searchParams;
-  });
+    hasNavigatedRef.current = false
+  })
 
-  const setSearchParams: SetSearchParams = useCallback((nextValue) => {
+  const setSearchParams: SetSearchParams = useCallback(nextValue => {
     const newParams =
-      typeof nextValue === "function"
-        ? nextValue(searchParamsRef.current)
-        : nextValue;
-
-    searchParamsRef.current = newParams;
+      typeof nextValue === "function" ?
+        nextValue(new URLSearchParams(window.location.search)) :
+        nextValue
 
     if (hasNavigatedRef.current) {
-      window.history.replaceState({}, "", `?${newParams}`);
+      window.history.replaceState({}, "", `?${newParams}`)
     } else {
-      window.history.pushState({}, "", `?${newParams}`);
+      window.history.pushState({}, "", `?${newParams}`)
     }
 
-    hasNavigatedRef.current = true;
-  }, []);
-  return [searchParams, setSearchParams];
-};
+    hasNavigatedRef.current = true
+  }, [])
+  return setSearchParams
+}
 
-export default useSearchParams;
+/**
+ * Like react `useState`, but for getting/setting search params in NextJS
+ * App Router. The setter is client-side only and will not trigger a call
+ * to the server. (Uses `window.history.pushState` internally, not `useRouter`).
+ *
+ * ```ts
+ * const [searchParams, setSearchParams] = useSearchParams();
+ * ```
+ * Where:
+ * - `searchParams` is the search params objet from `next/navigation`'s useSearchParams
+ * - `setSearchParams` is like React's `useState` setter:
+ *    - arg can be a single value or function (current => next)
+ *    - multiple synchronous calls to setSearchParams trigger only one pushState
+ */
+const useSearchParams = (): [URLSearchParams, SetSearchParams] => {
+  return [useNextSearchParams(), useSetSearchParams()]
+}
+
+export default useSearchParams
